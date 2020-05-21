966362d7c96d4b6a67b6bfe8a8a17b0ee1557f49
amarino <hussainamar@gmail.com>
2020-05-21 22:36:07 +0100

    Updates

---
+++ precache-manifest.02bed801035a2d059b4ee1101cc5f302.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1a821c48e5010a735cc90ea7a2fbc4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f040077a1a48478f66f",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c655a51673bcf606f747",
+    "url": "/static/js/2.e2ea4272.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e2ea4272.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f040077a1a48478f66f",
+    "url": "/static/js/main.c7f83827.chunk.js"
+  },
+  {
+    "revision": "ad591d0f4708f88e02d5",
+    "url": "/static/js/runtime-main.ca69820c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
